d8c5ae11fc6a18e09dde4330d4847f308b97c7f1
Payhaev17 <ran777dom@gmail.com>
2021-09-13 19:00:40 +0300

    changes

--- pages/basket.vue
+++ pages/basket.vue
@@ -1,7 +1,45 @@
 <template>
   <main class="basket">
     <div class="wrapper">
-      <h1>Basket</h1>
+      <the-empty-basket />
     </div>
   </main>
 </template>
+
+<style lang="scss" scoped>
+.empty-basket {
+  max-width: 540px;
+  min-height: 100%;
+
+  margin: 0 auto;
+
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -ms-flex-direction: column;
+  flex-direction: column;
+
+  &__title {
+    font-weight: 700;
+    font-size: 32px;
+  }
+
+  &__text {
+    color: #777777;
+
+    margin-top: 10px;
+
+    font-weight: 400;
+    font-size: 18px;
+  }
+
+  &__img {
+    margin-top: 50px;
+  }
+}
+</style>
